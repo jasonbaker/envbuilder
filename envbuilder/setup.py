9e4b3ae9607cb62dcf71525284eead63b6531e44
Jason Baker <amnorvend@gmail.com>
2010-01-13 14:36:00 -0600

    Adding virtualenv support back in and changing 'build' to 'setup'.

--- envbuilder/setup.py
+++ envbuilder/setup.py
@@ -4,8 +4,9 @@ from envbuilder.sh import sh
 
 class Setup(object):
     def run(self, args, config):
+        sh('virtualenv --no-site-packages .')
         for parcel in config.parcels:
-            build_cmds = parcel['build']
+            build_cmds = parcel['setup']
             if not isinstance(build_cmds, (list, tuple)):
                 build_cmds = [build_cmds]
             for build_cmd in build_cmds:
