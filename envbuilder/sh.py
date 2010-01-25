112155ce73d7f6b8d9f5f8c9055a2323c6034c0e
Jason Baker <amnorvend@gmail.com>
2010-01-25 10:27:17 -0600

    Improving the handling of failures a bit.

--- envbuilder/sh.py
+++ envbuilder/sh.py
@@ -1,12 +1,23 @@
-import subprocess
+import subprocess, sys
 
 def sh(cmd, cwd='.'):
     """
     A somewhat easier interface to subprocess.check_call.
     """
-    print '-->', cmd
+    notify(cmd)
     if cwd != '.':
         print '(From: %s)' % cwd
     # Eliminate empties
     cmd_list = [x for x in cmd.split() if x]
-    subprocess.check_call(cmd_list, cwd=cwd)
+    try:
+        subprocess.check_call(cmd_list, cwd=cwd)
+    except subprocess.CalledProcessError, e:
+        cmd = ' '.join(e.cmd)
+        returncode = e.returncode
+        notify('"%s" returned code %s' % (cmd, returncode))
+        notify('ABORTED')
+        sys.exit(returncode)
+
+def notify(cmd):
+    print '-->', cmd
+        
