151a85f11a21e9709e2eb83f3afce2c3f9255c09
Jason Baker <amnorvend@gmail.com>
2010-01-29 16:12:40 -0600

    Adding a test to make sure that __getitem__ on non-existant key raises a KeyError.

--- envbuilder/tests/test_config.py
+++ envbuilder/tests/test_config.py
@@ -1,6 +1,13 @@
+from nose.tools import raises
+
 from envbuilder.config import Config
 
 def test_getitem():
     stub_config = { 'foo' : 'bar' }
-    cfg = Config(stub_config)
+    cfg = Config(config=stub_config)
     assert cfg['foo'] == stub_config['foo']
+
+@raises(KeyError)
+def test_getitem_keyerror():
+    cfg = Config(config={'foo' : 'bar'})
+    cfg['asdf']
