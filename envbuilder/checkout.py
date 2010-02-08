429be211dba845b9b94b1466e285243db33c90c1
Jason Baker <amnorvend@gmail.com>
2010-02-08 10:15:29 -0600

    Making this capable of running multiple commands.

--- envbuilder/checkout.py
+++ envbuilder/checkout.py
@@ -2,9 +2,7 @@ from envbuilder.sh import sh
 
 class Checkout(object):
     def run(self, args, config):
-        for parcel in config.parcels:
-            cmd = parcel['checkout']
-            sh(cmd)
+        config.run_command('checkout', cwd='.')
             
     def add_args(self, subparsers):
         parser = subparsers.add_parser('checkout', help='Check out parcels.')
