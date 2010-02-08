44f82e77cddac659c48c6cb9fc01807abf0ee6c7
Jason Baker <amnorvend@gmail.com>
2010-02-08 08:53:44 -0600

    Merging changes in from github.

--- envbuilder/checkout.py
+++ envbuilder/checkout.py
@@ -3,8 +3,9 @@ from envbuilder.sh import sh
 class Checkout(object):
     def run(self, args, config):
         for parcel in config.parcels:
-            cmd = parcel['checkout']
-            sh(cmd)
+            cmd_list = parcel['checkout']
+            for cmd in cmd_list:
+                sh(cmd)
             
     def add_args(self, subparsers):
         parser = subparsers.add_parser('checkout', help='Check out parcels.')
