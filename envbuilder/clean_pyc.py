d6aa7eab4f15e51ebe01635023ec9bcba32e2300
Jason Baker <amnorvend@gmail.com>
2010-02-09 09:20:06 -0600

    Fixing this so it won't delete files in the virtualenv lib directory.

--- envbuilder/clean_pyc.py
+++ envbuilder/clean_pyc.py
@@ -4,7 +4,7 @@ import os
 
 class CleanPyc(object):
     def run(self, args, config):
-        clean_pyc (config.parcel_names)
+        clean_pyc (config.parcel_dirs)
 
     def add_args(self, subparsers):
         parser = subparsers.add_parser('clean_pyc',
@@ -17,7 +17,7 @@ def clean_pyc(paths):
     print 'Deleting all .pyc files in project subdirectories'
     target_paths = []
     for path in paths:
-        assert os.path.isdir(path)
+        assert os.path.isdir(path), '%s is not a directory' % path
         target_paths +=  gather_directories_recursively(path)
     for path in target_paths:
         filenames = os.listdir(path)
@@ -33,7 +33,9 @@ def gather_directories_recursively (fpath):
     def append_dirs (junk, dirpath, nameList):
         for name in nameList:
             fullpath = os.path.join(dirpath,name)
-            if os.path.isdir(fullpath):
+            # We don't want to look at non-directory files right now
+            # Also, don't delete files from the virtualenv lib directory
+            if os.path.isdir(fullpath) and 'python2.6' not in fullpath:
                 pathList.append (fullpath)
     os.path.walk(fpath, append_dirs, None)
     return pathList
