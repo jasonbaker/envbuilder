f31e94294d2bf4ac01b37822115f5c17f0beae98
Jason Baker <amnorvend@gmail.com>
2010-03-24 13:07:44 -0500

    Reverting this back to the old argument parser.  It worked better.

--- envbuilder/args.py
+++ envbuilder/args.py
@@ -1,28 +1,21 @@
 import sys
 
-import argparse
-
-import envbuilder
-
 class Arguments(object):
-    def __init__(self, args=None):
-        if args is None:
-            try:
-                args = sys.argv[1:]
-            except IndexError:
-                args = []
+    def __init__(self, args=sys.argv):
         self.args = args
-        self.parser = argparse.ArgumentParser()
-        self.parser.add_argument('command', default='help',
-                                 help='The command to run (see below for common commands)',
-                                 nargs='?',)
-        self.parser.add_argument('--version', action='version',
-                            version=envbuilder.__version__,
-                            help='Get the version number and quit')
-        options, remaining = self.parser.parse_known_args(self.args)
-        self.command = options.command
-        self.arguments = remaining
 
-    def print_help(self):
-        self.parser.print_help()
-        print '\n'
+    @property
+    def command(self):
+        try:
+            return self.args[1]
+        except IndexError:
+            # This is an empty list, so return nothing.
+            return ''
+
+    @property
+    def arguments(self):
+        try:
+            return self.args[2:]
+        except IndexError:
+            # This is an empty list, so return an empty list
+            return []
