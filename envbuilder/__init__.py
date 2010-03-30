f3916127d8bd7e3ffed925fc0793fb5de0ca5c65
Jason Baker <amnorvend@gmail.com>
2010-03-30 14:14:47 -0500

    Removing an erroneous import statement

--- envbuilder/__init__.py
+++ envbuilder/__init__.py
@@ -1,6 +1,5 @@
 from envbuilder.checkout import Checkout
 from envbuilder.setup import Setup
-from envbuilder.clean_pyc import CleanPyc
 from envbuilder.help import Help
 
 __version__ = '0.3.0.dev'
